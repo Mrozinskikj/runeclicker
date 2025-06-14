81c499a0900fa54a191de25cb34389777b36776a
Krzysztof Mrozinski <60255374+Mrozinskikj@users.noreply.github.com>
2025-06-14 20:00:13 +0900

    fix build error

--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import { useScale } from "../logic/useScale";
 
 interface TooltipProps {
